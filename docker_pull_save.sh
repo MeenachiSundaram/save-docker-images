98aeb38a35baf32ede21b18ed886d92970209264
MeenachiSundaram V <meenachisundaram.v@gmail.com>
2016-11-14 16:25:58 +0530

    Update

--- docker_pull_save.sh
+++ docker_pull_save.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
 
+#name=($@)
+name=(`cat Docker_pull_list`)
+
+for (( i=0;i<${#name[@]}; i++ ))
+        do
+                docker pull ${name[$i]}
+                if [ $? -eq 0 ]; then
+                        rename=$(sed 's/-/_/g' <<< ${name[$i]} )
+                        echo $rename
+                        docker save ${name[$i]} $rename.tar
+                fi
+done
